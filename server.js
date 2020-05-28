df207850f761054bcb6b4d1a2c9862c311f2ca3d
Samsul Fauzi <40360275+aloelsafzi@users.noreply.github.com>
2020-05-28 15:48:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -71,4 +71,4 @@ io.on('connection', socket => {
 });
 
 const PORT = 3000 || process.env.PORT;
-server.listen(PORT, () => { console.log(`Server run on localhost:${PORT}`); });
+server.listen(PORT, console.log(`Server run on localhost:${PORT}`));
